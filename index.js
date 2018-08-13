d8569443873db8a93bef90f420acb9467c03d8cc
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-13 05:25:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const driver = {};
    return driver;
  }
  function deleteFromDriverByKey(driver, key){
-   const newObj = Object.assign({}, driver;
+   const newObj = Object.assign({}, driver);
    delete newObj[key];
  }
