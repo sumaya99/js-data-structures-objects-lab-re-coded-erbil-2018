60751497bb95e3b6ff54fc45408c35d69d204d95
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-13 05:20:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const driver = {};
  function updateDriverWithKeyAndValue(driver, key, value)
  {  return Object.assign({}, driver,{[key]: value})}
+ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){}
