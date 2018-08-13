ad6616697dd372d54a076f39931344f8af6b97ec
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-13 05:16:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Write your solution in this file!
 const driver = {};
- function updateDriverWithKeyAndValue(driver, 'address', '11 Broadway'){bame:
-  
-}
+ function updateDriverWithKeyAndValue(driver, 'address', '11 Broadway')
+ {  name: 'Sam', address: '12 Broadway'}
